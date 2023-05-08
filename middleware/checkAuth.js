6316316762b8ffce5ae24943d5fbb5fd89e71e44
enders-o <ender_sotelo@outlook.com>
2023-05-08 10:51:59 -0700

    Basic login and register functionality finished, added authentication and sessions as well

---
+++ middleware/checkAuth.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ensureAuthenticated: function (req, res, next) {
+      console.log(req)
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect("/login");
+    },
+    forwardAuthenticated: function (req, res, next) {
+      // console.log(req.user)
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect("/teams");
+    },
+  };
+  
