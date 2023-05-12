9ee7e5cd38a5e75813bc630aa76ec5aa97e0028d
enders-o <ender_sotelo@outlook.com>
2023-05-11 20:37:48 -0700

    registration page test

--- test/user.test.js
+++ test/user.test.js
@@ -16,3 +16,16 @@ it("login page", async () => {
     })
     .expect(200);
 });
+
+it("registration page", async () => {
+    await request(server)
+    .get("/register")
+    .expect(res => {
+        res.text.includes('email');
+        res.text.includes('password');
+        res.text.includes('firstName');
+        res.text.includes('lastName');
+        res.text.includes('gender');
+    })
+    .expect(200);
+});
